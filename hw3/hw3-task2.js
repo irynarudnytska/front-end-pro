48c52b16d3f8b7e7e8f08c453e6943d67316ff63
Iryna Rudnytska <irroudnytska@gmail.com>
2026-03-01 19:01:10 +0100

    Task 3.2

---
+++ hw3/hw3-task2.js
@@ -0,0 +1,4 @@
+const firstInput = prompt("Type something magical ✨");
+const secondInput = prompt("Type something magical ✨");
+const thirdInput = prompt("Type something magical ✨");
+console.log(`Full user input: ${firstInput}, ${secondInput}, ${thirdInput}`);
