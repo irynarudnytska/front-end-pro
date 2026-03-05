c4d097ec40a47d42b560d352a41d0b265771ef67
Iryna Rudnytska <irroudnytska@gmail.com>
2026-03-06 00:06:53 +0100

    fix/task 4.3

--- hw4/hw4-task3.js
+++ hw4/hw4-task3.js
@@ -1,6 +1,17 @@
 const userYearBirth = prompt("Enter your year of birth");
+if (userYearBirth === null) {
+  alert("It's a pity you chose not to enter your year of birth");
+}
+
 const userCity = prompt("Enter the city you live in");
+if (userCity === null) {
+  alert("It's a pity you chose not to enter your city");
+}
+
 const userFavoriteSport = prompt("Enter your favorite sport");
+if (userFavoriteSport === null) {
+  alert("It's a pity you chose not to enter your favorite sport");
+}
 
 const userAge = new Date().getFullYear() - Number(userYearBirth);
 
@@ -16,13 +27,7 @@ const championBySport = {
   basketball: "Michael Jordan",
 };
 
-if (userYearBirth === null) {
-  alert("It's a pity you chose not to enter your year of birth");
-} else if (userCity === null) {
-  alert("It's a pity you chose not to enter your city");
-} else if (userFavoriteSport === null) {
-  alert("It's a pity you chose not to enter your favorite sport");
-} else {
+if (userYearBirth !== null && userCity !== null && userFavoriteSport !== null) {
   alert(
     `Hello, you are ${userAge} years old. ${
       userCity in countryByCapital
