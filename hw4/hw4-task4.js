af9e4bd3d9c7a6b20c6a473314a8e5d00af4b638
Iryna Rudnytska <irroudnytska@gmail.com>
2026-03-05 23:35:15 +0100

    task 4.4

--- hw4/hw4-task4.js
+++ hw4/hw4-task4.js
@@ -1,13 +1,15 @@
-let numOrStr = prompt('input number or string');
-console.log(numOrStr)
+let numOrStr = prompt("input number or string");
 
-
-if (numOrStr === null) {
-    console.log('ви скасували')
-} else if (numOrStr.trim() === '') {
-    console.log('Empty String');
-} else if (isNaN(+numOrStr)) {
-    console.log(' number is Ba_NaN')
-} else {
-    console.log('OK!')
+switch (true) {
+  case numOrStr === null:
+    console.log("You canceled");
+    break;
+  case numOrStr.trim() === "":
+    console.log("Empty String");
+    break;
+  case isNaN(+numOrStr):
+    console.log("number is Ba_NaN");
+    break;
+  default:
+    console.log("OK!");
 }
