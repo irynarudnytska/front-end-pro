6c607bc73b4296389ad3528e7bed28e3b7d5b3d6
Iryna Rudnytska <irroudnytska@gmail.com>
2026-03-07 22:55:19 +0100

    Task 5.3

---
+++ hw5/hw5-task3.js
@@ -0,0 +1,8 @@
+const inputNumber = Number(prompt("Enter a number"));
+const arrayNumbers = [];
+for (let i = 1; i <= 100; i++) {
+  if (i ** 2 <= inputNumber) {
+    arrayNumbers.push(i);
+  }
+}
+console.log(arrayNumbers);
